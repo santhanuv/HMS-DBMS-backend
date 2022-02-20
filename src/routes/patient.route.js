15d76904cb1d66ae00045da48edfc068e914bb16
santhanuv17 <santhanuv17@gmail.com>
2022-02-20 12:04:38 +0530

    get latest appointments completed, doctor appointemnts completed

--- src/routes/patient.route.js
+++ src/routes/patient.route.js
@@ -1,7 +1,12 @@
 const patientController = require("../controllers/patient.controller");
+const appointmentController = require("../controllers/appointment.controller");
 const router = require("express").Router();
 const createUser = require("../middleware/createUserMiddleware");
 
 router.post("/", createUser, patientController.createPatientHandler);
+router.get(
+  "/appointments",
+  appointmentController.getAllAppointmentsHandler("Patient")
+);
 
 module.exports = router;
