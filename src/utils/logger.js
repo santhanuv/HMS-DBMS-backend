71648f516629fac0485db30ce356e40ba137d423
santhanuv17 <santhanuv17@gmail.com>
2022-02-20 14:25:04 +0530

    pino-pretty error checking

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,12 @@
-const logger = require("pino")({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: true,
-    },
+const transport = {
+  options: {
+    colorize: true,
+    translateTime: true,
   },
+};
+process.env.NODE_ENV !== "production" && (transport.target = "pino-pretty");
+
+const logger = require("pino")({
+  transport,
 });
 module.exports = logger;
