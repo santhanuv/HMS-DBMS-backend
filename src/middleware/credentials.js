66d147d5ec39ada23574ebcb1ec5f38fdd544055
santhanuv17 <santhanuv17@gmail.com>
2022-02-20 15:52:03 +0530

    cors error

--- src/middleware/credentials.js
+++ src/middleware/credentials.js
@@ -2,6 +2,7 @@ const whiteList = require("../config/cors/whiteList");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
+  console.log(req.headers, process.env.NODE_ENV);
   if (whiteList.indexOf(origin) > -1) {
     res.header("Access-Control-Allow-Credentials", true);
   }
